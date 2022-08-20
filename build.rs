4abd1597d8841fbb5b10ef25ab57ae6f3e00bc50
mkn <mokona@riseup.net>
2022-08-20 14:52:27 +0800

    v0.8.7 release & try refining the git sha display

--- build.rs
+++ build.rs
@@ -1,9 +1,17 @@
 fn git_commit_hash() -> String {
     use std::process::Command;
-    let hash: String = String::from_utf8( Command::new("git").arg("rev-parse").arg("origin/master").output().unwrap().stdout ).unwrap();
+    let mut hash: String = String::from_utf8( Command::new("git").arg("rev-parse").arg("origin/master").output().unwrap().stdout ).unwrap();
+
+    if hash.is_empty() {
+        hash = "release".to_string();
+    } else {
+        hash = "git.".to_string() + &hash;
+    }
+
     hash
 }
 
 fn main() {
     println!("cargo:rustc-env=GIT_COMMIT_HASH={}", git_commit_hash());
 }
+
