c05755e7220827bb68b2625e28567d5f74d18ff1
mkn <mokona@riseup.net>
2022-08-21 17:18:19 +0800

    add git commit hash for compile-time generated (run gitsha-incode.py before cargo-release, avoid empty)

--- build.rs
+++ build.rs
@@ -1,4 +1,20 @@
 fn git_commit_hash() -> String {
+    #[allow(unused_variables)]
+
+    let static_git_sha = "[[Replaceit]]";
+let static_git_sha = "git.dc2baa379cee3ae84a51cbb95e9ae6f8b43e8c9e"; //CODEADD// by gitsha in code
+
+    if static_git_sha.contains("[[Replaceit]]") {
+        return _dyna_git_commit_hash();
+    }
+    if static_git_sha == "git." {
+        return "release-nogit?".to_string();
+    }
+
+    assert!( static_git_sha.starts_with("git.") || static_git_sha == "release" );
+    static_git_sha.to_string()
+}
+fn _dyna_git_commit_hash() -> String {
     use std::process::Command;
     let mut hash: String = String::from_utf8( Command::new("git").arg("rev-parse").arg("origin/master").output().unwrap().stdout ).unwrap();
 
---
+++ gitsha-incode.py
@@ -0,0 +1,25 @@
+import os
+
+gitsha = os.popen('git rev-parse origin/master').read().strip()
+print('GIT CURRENT SHA', repr(gitsha))
+
+assert len( bytes.fromhex(gitsha) ) == 20
+
+rs = 'build.rs'
+
+d = open(rs, 'r').read()
+ds = d.split('\n')
+
+ptr = 0
+while '//CODEADD//' not in ds[ptr]:
+    ptr += 1
+print('PTR', ptr)
+
+ds[ptr] = 'let static_git_sha = "git.%s"; //CODEADD// by gitsha in code' % (gitsha,)
+
+d2 = '\n'.join(ds)
+print(ds)
+
+open(rs, 'w').write(d2)
+os.sync()
+
